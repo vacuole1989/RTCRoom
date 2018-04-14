638f4d0b6aefa2baa80137d6692f7962c8fff205
git <cxd1989928>
2018-04-14 21:05:27 +0800

    qqq

---
+++ RTCRoomServer/src/main/java/com/cxd/rtcroom/dao/UserInfoRepository.java
@@ -0,0 +1,8 @@
+package com.cxd.rtcroom.dao;
+
+import com.cxd.rtcroom.bean.UserInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserInfoRepository extends CrudRepository<UserInfo, String> {
+
+}
